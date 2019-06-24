485c5a9c54628e51a0c108bb269b5669590722bb
Katlin Anderson and lindsey baker <lindsey@>
2019-06-24 11:45:51 -0700

    Add sample database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent
   ],
   imports: [
     BrowserModule,
