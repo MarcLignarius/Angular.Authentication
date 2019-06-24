db0769fa1c5af3fb2ac6f580908897c62df04b90
Katlin Anderson and lindsey baker <katlin@>
2019-06-24 10:34:42 -0700

    Initial commit

---
+++ src/environments/api-keys.ts
@@ -0,0 +1,9 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyDowvXlwfIXksuJtsLZhsrnFuJlmvhFNJI",
+    authDomain: "telestrations-bb0da.firebaseapp.com",
+    databaseURL: "https://telestrations-bb0da.firebaseio.com",
+    projectId: "telestrations-bb0da",
+    storageBucket: "gs://telestrations-bb0da.appspot.com",
+    messagingSenderId: "33437725891",
+    appId: "1:33437725891:web:56d9276d63a575cd"
+  };
