cc9bc9b7f3a9836bb1158c536aa062d5966c3659
lindsey baker <lindsey@>
2019-06-25 15:02:20 -0700

    Add routes to login, signup, email and members

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,14 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PlayerListComponent } from './player-list/player-list.component';
 import { ProfileComponent } from './profile/profile.component';
 
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
+import { SignupComponent } from './signup/signup.component';
+import { EmailComponent } from './email/email.component';
+import { MembersComponent } from './members/members.component';
+
+
 const appRoutes: Routes = [
     {
         path: '',
@@ -17,7 +25,22 @@ const appRoutes: Routes = [
     {
         path: 'playerlists/:id',
         component: ProfileComponent
+    },
+    { 
+        path: 'login',
+        component: LoginComponent 
+    },
+    {
+         path: 'signup', component: SignupComponent },
+    {
+         path: 'login-email', component: EmailComponent },
+    {
+         path: 'members',
+         component: MembersComponent, 
+         canActivate: [AuthService] 
     }
+
+
 
 
  ];
